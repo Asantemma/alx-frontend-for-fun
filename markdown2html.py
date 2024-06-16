884a47ca069284de3e7088bbdcbf2b59f3879ac8
Emmanuel Asante <asantemma7@gmail.com>
2024-06-16 11:23:38 +0000

    WENT with file 2

--- markdown2html.py
+++ markdown2html.py
@@ -1,158 +1,98 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
+"""
+Converts markdown text to html.
+"""
 import sys
-import os
+import os.path
 import re
 import hashlib
 
-
-def convert_markdown_to_html(markdown_text):
-    html_lines = []
-    lines = markdown_text.split('\n')
-    in_unordered_list = False
-    in_ordered_list = False
-    in_paragraph = False
-    paragraph_lines = []
-
-    for line in lines:
-        # Convert **text** to <b>text</b>
-        line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
-        # Convert __text__ to <em>text</em>
-        line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
-        # Convert [[text]] to MD5 hash
-        line = re.sub(
-            r'\[\[(.*?)\]\]',
-            lambda m: hashlib.md5(m.group(1).encode()).hexdigest(),
-            line
-        )
-        # Convert ((text)) by removing all 'c' and 'C'
-        line = re.sub(
-            r'\(\((.*?)\)\)',
-            lambda m: m.group(1).replace('c', '').replace('C', ''),
-            line
-        )
-
-        if line.startswith('#'):
-            if in_unordered_list:
-                html_lines.append('</ul>')
-                in_unordered_list = False
-            if in_ordered_list:
-                html_lines.append('</ol>')
-                in_ordered_list = False
-            if in_paragraph:
-                html_lines.append(
-                    '<br />\n    '.join(paragraph_lines) + '\n</p>'
-                )
-                in_paragraph = False
-                paragraph_lines = []
-            heading_level = len(line.split(' ')[0])
-            heading_text = line[heading_level:].strip()
-            if 1 <= heading_level <= 6:
-                html_lines.append(
-                    f'<h{heading_level}>{heading_text}</h{heading_level}>'
-                )
-            else:
-                html_lines.append(line)
-        elif line.startswith('- '):
-            if in_ordered_list:
-                html_lines.append('</ol>')
-                in_ordered_list = False
-            if in_paragraph:
-                html_lines.append(
-                    '<br />\n    '.join(paragraph_lines) + '\n</p>'
-                )
-                in_paragraph = False
-                paragraph_lines = []
-            if not in_unordered_list:
-                html_lines.append('<ul>')
-                in_unordered_list = True
-            list_item = line[2:].strip()
-            html_lines.append(f'    <li>{list_item}</li>')
-        elif line.startswith('* '):
-            if in_unordered_list:
-                html_lines.append('</ul>')
-                in_unordered_list = False
-            if in_paragraph:
-                html_lines.append(
-                    '<br />\n    '.join(paragraph_lines) + '\n</p>'
-                )
-                in_paragraph = False
-                paragraph_lines = []
-            if not in_ordered_list:
-                html_lines.append('<ol>')
-                in_ordered_list = True
-            list_item = line[2:].strip()
-            html_lines.append(f'    <li>{list_item}</li>')
-        elif line.strip() == '':
-            if in_paragraph:
-                html_lines.append(
-                    '<br />\n    '.join(paragraph_lines) + '\n</p>'
-                )
-                in_paragraph = False
-                paragraph_lines = []
-            if in_unordered_list:
-                html_lines.append('</ul>')
-                in_unordered_list = False
-            if in_ordered_list:
-                html_lines.append('</ol>')
-                in_ordered_list = False
-        else:
-            if in_unordered_list:
-                html_lines.append('</ul>')
-                in_unordered_list = False
-            if in_ordered_list:
-                html_lines.append('</ol>')
-                in_ordered_list = False
-            if not in_paragraph:
-                paragraph_lines.append('<p>')
-                paragraph_lines.append('    ' + line.strip())
-                in_paragraph = True
-            else:
-                paragraph_lines.append('    ' + line.strip())
-
-    if in_paragraph:
-        html_lines.append('<br />\n    '.join(paragraph_lines) + '\n</p>')
-    if in_unordered_list:
-        html_lines.append('</ul>')
-    if in_ordered_list:
-        html_lines.append('</ol>')
-
-    return '\n'.join(html_lines)
-
-
-def main():
+if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print(
-            "Usage: ./markdown2html.py README.md README.html",
-            file=sys.stderr
-        )
-        sys.exit(1)
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        exit(1)
 
-    markdown_file = sys.argv[1]
-    html_file = sys.argv[2]
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
 
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
+    with open(sys.argv[1]) as input_file:
+        with open(sys.argv[2], 'w') as output_file:
+            ul_open, ol_open, in_paragraph = False, False, False
+            # bold syntax
+            for current_line in input_file:
+                current_line = current_line.replace('**', '<b>', 1)
+                current_line = current_line.replace('**', '</b>', 1)
+                current_line = current_line.replace('__', '<em>', 1)
+                current_line = current_line.replace('__', '</em>', 1)
 
-    try:
-        with open(markdown_file, 'r', encoding='utf-8') as f:
-            md_content = f.read()
-    except IOError as e:
-        print(f"Error reading {markdown_file}: {e}", file=sys.stderr)
-        sys.exit(1)
+                # md5
+                md5_matches = re.findall(r'\[\[.+?\]\]', current_line)
+                md5_content = re.findall(r'\[\[(.+?)\]\]', current_line)
+                if md5_matches:
+                    current_line = current_line.replace(
+                        md5_matches[0], hashlib.md5(md5_content[0].encode()).hexdigest())
 
-    html_content = convert_markdown_to_html(md_content)
+                # remove the letter C
+                c_matches = re.findall(r'\(\(.+?\)\)', current_line)
+                c_content = re.findall(r'\(\((.+?)\)\)', current_line)
+                if c_matches:
+                    no_c_content = ''.join(ch for ch in c_content[0]
+                                           if ch not in 'Cc')
+                    current_line = current_line.replace(c_matches[0],
+                                                        no_c_content)
 
-    try:
-        with open(html_file, 'w', encoding='utf-8') as f:
-            f.write(html_content)
-    except IOError as e:
-        print(f"Error writing {html_file}: {e}", file=sys.stderr)
-        sys.exit(1)
+                line_length = len(current_line)
+                heading_content = current_line.lstrip('#')
+                heading_level = line_length - len(heading_content)
+                ul_content = current_line.lstrip('-')
+                ul_level = line_length - len(ul_content)
+                ol_content = current_line.lstrip('*')
+                ol_level = line_length - len(ol_content)
+                # headings, lists
+                if 1 <= heading_level <= 6:
+                    current_line = '<h{}>'.format(
+                        heading_level) + heading_content.strip()
+                    + '</h{}>\n'.format(
+                        heading_level)
 
-    sys.exit(0)
+                if ul_level:
+                    if not ul_open:
+                        output_file.write('<ul>\n')
+                        ul_open = True
+                    current_line = '<li>' + ul_content.strip() + '</li>\n'
+                if ul_open and not ul_level:
+                    output_file.write('</ul>\n')
+                    ul_open = False
 
+                if ol_level:
+                    if not ol_open:
+                        output_file.write('<ol>\n')
+                        ol_open = True
+                    current_line = '<li>' + ol_content.strip() + '</li>\n'
+                if ol_open and not ol_level:
+                    output_file.write('</ol>\n')
+                    ol_open = False
 
-if __name__ == "__main__":
-    main()
+                if not (heading_level or ul_open or ol_open):
+                    if not in_paragraph and line_length > 1:
+                        output_file.write('<p>\n')
+                        in_paragraph = True
+                    elif line_length > 1:
+                        output_file.write('<br/>\n')
+                    elif in_paragraph:
+                        output_file.write('</p>\n')
+                        in_paragraph = False
+
+                if line_length > 1:
+                    output_file.write(current_line)
+
+            if ul_open:
+                output_file.write('</ul>\n')
+            if ol_open:
+                output_file.write('</ol>\n')
+            if in_paragraph:
+                output_file.write('</p>\n')
+    exit(0)
